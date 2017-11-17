edc53d752d62e6b2846f160d49d095943211a6bc
Chris Marks <chrismarksus@gmail.com>
2017-11-17 15:04:52 -0500

    Add checks

--- check_server.sh
+++ check_server.sh
@@ -1,3 +1,12 @@
-#!bin/bash
+#!/bin/bash
 
-curl -o /dev/null --silent --head --write-out '%{http_code} $0
+URL="$1"
+STATUS_CODE=$(curl -sL -w "%{http_code}\\n" "$URL" -o /dev/null)
+
+if [ "$STATUS_CODE" != "200" ]; then
+        echo "SERVER CHECK: host $URL failed, status code $STATUS_CODE"
+    exit 0
+else
+        echo "SERVER CHECK: host $URL success status code $STATUS_CODE"
+    exit 1
+fi
